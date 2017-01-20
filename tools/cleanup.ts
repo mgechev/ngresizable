8bb268337550570e22e1200535abae530cab6f5f
mgechev <mgechev@gmail.com>
2017-01-20 14:40:07 -0800

    chore: drop scripts

--- tools/cleanup.ts
+++ tools/cleanup.ts
@@ -2,4 +2,5 @@ import { writeFileSync, readFileSync } from 'fs';
 
 const packageJson = JSON.parse(readFileSync('./dist/package.json').toString());
 delete packageJson.devDependencies;
+delete packageJson.scripts;
 writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));
