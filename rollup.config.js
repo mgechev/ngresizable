65783b41e37ed8c952b2951c40691fefeac5e3cd
Raven Demeyer <raven.demeyer@gmail.com>
2017-02-12 10:37:07 +0100

    Suppress rollup warnings

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,14 @@ export default {
   external: [
     '@angular/core',
     '@angular/common'
-  ]
+  ],
+  onwarn: ( warning ) => {
+      const skip_codes = [
+          'THIS_IS_UNDEFINED',
+          'MISSING_GLOBAL_NAME'
+      ];
+      if ( skip_codes.indexOf(warning.code) != -1 ) return;
+      console.error(warning);
+  }
 };
 
